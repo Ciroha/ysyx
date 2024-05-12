e7b826289a44a3eafd24b039887d3697afab34bd
tracer-ysyx <tracer@ysyx.org>
2024-05-12 12:37:20 +0800

    > compile NEMU ysyx_22040000 曹逸飞 Linux ciroha-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:37:20 up  1:25,  3 users,  load average: 0.10, 0.04, 0.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,6 +55,11 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args) {
+  cpu_exec(1);
+  return 0;
+}
+
 static struct {
   const char *name;
   const char *description;
@@ -63,7 +68,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si", "single step", cmd_si },
   /* TODO: Add more commands */
 
 };
