4a95343b68ff0041d68c59ab4a40e217aea524a5
tracer-ysyx <tracer@ysyx.org>
2024-06-18 19:37:38 +0800

    > compile NEMU ysyx_22040000 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:37:38 up  9:40,  3 users,  load average: 0.22, 0.20, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -140,6 +140,7 @@ static bool make_token(char *e) {
 				    tokens[nr_token++].type = TK_RIGHT;
 				    break;
 			    case TK_NOTYPE:
+            tokens[nr_token++].type = TK_NOTYPE;
 				    break; //不对空格进行记录
 			    case TK_NUMBER:
 				    tokens[nr_token].type = TK_NUMBER;
