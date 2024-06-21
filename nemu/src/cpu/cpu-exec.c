db013c646a88e98ce04895b4533bd5bd55ca1987
tracer-ysyx <tracer@ysyx.org>
2024-06-21 10:03:20 +0800

    > compile NEMU ysyx_22040000 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:03:20 up  1:03,  3 users,  load average: 0.01, 0.04, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,7 +40,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   
-  wp_difftest();
+  IFDEF(CONFIG_WATCHPOINT, wp_difftest();)
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
