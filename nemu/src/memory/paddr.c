16f9766776eee13a7791a1d731e1acd65a7f83c5
tracer-ysyx <tracer@ysyx.org>
2024-07-12 14:19:18 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:19:18 up  3:08,  0 users,  load average: 0.20, 0.25, 0.18

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,7 +51,12 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  if (likely(in_pmem(addr))) {
+    word_t mem_tmp;
+    mem_tmp = pmem_read(addr, len);
+    printf("Address is: %x, mem is %x\n", addr, mem_tmp);
+    return mem_tmp;
+  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
