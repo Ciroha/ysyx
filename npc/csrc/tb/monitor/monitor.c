dc08d994a22c1df36ed73d2f7a9892c41f37871f
tracer-ysyx <tracer@ysyx.org>
2024-07-18 17:44:13 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:44:13 up  7:05,  1 user,  load average: 0.43, 0.31, 0.25

--- npc/csrc/tb/monitor/monitor.c
+++ npc/csrc/tb/monitor/monitor.c
@@ -75,16 +75,3 @@ uint32_t pmem_read(uint32_t *memory, uint32_t vaddr){
 	uint32_t *paddr = guest_to_host(memory, vaddr);
 	return *paddr;
 }
-
-uint32_t *init_mem(size_t size) {
-	uint32_t* memory = (uint32_t*)malloc(size * sizeof(uint32_t));
-	memcpy(memory,img,sizeof(img));
-	if(memory == NULL) {exit(0);}
-	return memory;
-}
-
-
-uint32_t pmem_read(uint32_t *memory, uint32_t vaddr){
-	uint32_t *paddr = guest_to_host(memory, vaddr);
-	return *paddr;
-}
