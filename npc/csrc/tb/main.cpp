fe89d69ed8c7a3a58269e04a70ac34762cf81fcc
tracer-ysyx <tracer@ysyx.org>
2024-07-18 14:18:11 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:18:11 up  3:39,  0 users,  load average: 0.35, 0.26, 0.18

--- npc/csrc/tb/main.cpp
+++ npc/csrc/tb/main.cpp
@@ -12,30 +12,30 @@ static Vysyx_23060332_top dut;
 VerilatedContext* contextp = NULL;                                                                                        
 VerilatedVcdC* tfp = NULL;
 static char *img_file = NULL;
+static uint32_t *pmem = NULL;
 
-// static uint32_t img[] = {
-// 	// 0b00000000010100000000000010010011, //addi x1 x0 5 0x80000000
-// 	// 0b00000000000100000000000100010011, //addi x2 x0 1 0x80000004
-// 	// 0b00000000001000000000000100010011, //addi x2 x0 2 0x80000008
-// 	// 0b00000000010100001000000100010011, //addi x2 x1 5 0x8000000c
-// 	// 0b00000000000000000001000110010111,	//auipc x3, 1  0x80000010
-// 	// 0b10000000000000000000001000110111,	//lui x4, 0x80000
-// 	// 0b00000000000000100000001001100111, //jalr x4, 0(x4)
-// 	// 0b00000000000100000000000001110011,	//ebreak
-// 	0x00000413,
-// 	0x00009117,
-// 	0xffc10113,
-// 	0x00c000ef,
-// 	0x00000513,
-// 	0x00008067,
-// 	0xff410113,
-// 	0x00000517,
-// 	0x01450513,
-// 	0x00112423,
-// 	0xfe9ff0ef,
-// 	0x0000006f,
-// };
-static uint8_t *img = NULL;
+static uint32_t img[] = {
+	// 0b00000000010100000000000010010011, //addi x1 x0 5 0x80000000
+	// 0b00000000000100000000000100010011, //addi x2 x0 1 0x80000004
+	// 0b00000000001000000000000100010011, //addi x2 x0 2 0x80000008
+	// 0b00000000010100001000000100010011, //addi x2 x1 5 0x8000000c
+	// 0b00000000000000000001000110010111,	//auipc x3, 1  0x80000010
+	// 0b10000000000000000000001000110111,	//lui x4, 0x80000
+	// 0b00000000000000100000001001100111, //jalr x4, 0(x4)
+	// 0b00000000000100000000000001110011,	//ebreak
+	0x00000413,
+	0x00009117,
+	0xffc10113,
+	0x00c000ef,
+	0x00000513,
+	0x00008067,
+	0xff410113,
+	0x00000517,
+	0x01450513,
+	0x00112423,
+	0xfe9ff0ef,
+	0x0000006f,
+};
 
 
 
@@ -78,7 +78,7 @@ static size_t load_img(){
 	printf("The image is %s, size = %ld", img_file, size);
 
 	fseek(fp, 0 , SEEK_SET);
-	int ret = fread(img, size, 1, fp);
+	int ret = fread(guest_to_host(NULL, 0x80000000), size, 1, fp);
 	assert(ret == 1);
 
 	fclose(fp);
@@ -86,16 +86,16 @@ static size_t load_img(){
 }
 
 uint32_t *init_mem(size_t size) {
-	uint32_t* memory = (uint32_t*)malloc(size * sizeof(uint8_t));
+	uint32_t* memory = (uint32_t*)malloc(size * sizeof(uint32_t));
 	memcpy(memory,img,sizeof(img));
 	if(memory == NULL) {exit(0);}
 	return memory;
 }
 
-uint32_t guest_to_host(uint32_t addr){return addr-0x80000000;}
+uint32_t *guest_to_host(uint32_t *memory, uint32_t addr){return memory + (addr-0x80000000)/4;}
 uint32_t pmem_read(uint32_t *memory, uint32_t vaddr){
-	uint32_t paddr = guest_to_host(vaddr);
-	return memory[paddr/4];
+	uint32_t *paddr = guest_to_host(memory, vaddr);
+	return *paddr;
 }
 
 void single_cycle(){
@@ -125,7 +125,7 @@ int main(int argc, char *argv[]){
 	printf("Parsing arguments!");
 	parse_args(argc, argv);
 	printf("image file is %s", img_file);
-	uint32_t *memory;
+	uint32_t *memory = NULL;
 	size_t size = load_img();
 	memory = init_mem(size);
 
@@ -140,7 +140,7 @@ int main(int argc, char *argv[]){
 		dut.clk=0;dut.eval();		
 		tfp->dump(contextp -> time());
 		contextp -> timeInc(1);
-		dut.inst = pmem_read(memory,dut.pc);
+		dut.inst = pmem_read(memory, dut.pc);
 		dut.clk=1;dut.eval();		
 		tfp->dump(contextp -> time());
 		contextp -> timeInc(1);
