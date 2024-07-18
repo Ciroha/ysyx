535e3e2d3cdf818db270cb0112d2d1f47cb91393
tracer-ysyx <tracer@ysyx.org>
2024-07-18 21:06:10 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:06:10 up 10:27,  1 user,  load average: 0.51, 0.42, 0.35

--- npc/csrc/tb/include/memory.h
+++ npc/csrc/tb/include/memory.h
@@ -11,7 +11,7 @@
 static char *img_file = NULL;
 
 uint32_t *guest_to_host(uint32_t addr);
-uint32_t *init_mem(size_t size);
+void init_mem(size_t size);
 uint32_t pmem_read(uint32_t vaddr);
 
 #endif
