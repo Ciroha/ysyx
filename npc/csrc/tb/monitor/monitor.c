384738deba903482cbe61db2e579c9a70c95e74b
tracer-ysyx <tracer@ysyx.org>
2024-07-18 18:28:45 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:28:45 up  7:50,  1 user,  load average: 0.25, 0.24, 0.26

--- npc/csrc/tb/monitor/monitor.c
+++ npc/csrc/tb/monitor/monitor.c
@@ -4,8 +4,8 @@
 
 
 uint32_t *guest_to_host(uint32_t *memory, uint32_t addr);
-static int parse_args(int argc, char *argv[]);
-static size_t load_img(uint32_t *memory);
+// static int parse_args(int argc, char *argv[]);
+// static size_t load_img(uint32_t *memory);
 uint32_t *init_mem(size_t size);
 uint32_t pmem_read(uint32_t *memory, uint32_t vaddr);
 
@@ -19,53 +19,53 @@ static uint32_t img[] = {
 	0b00000000000100000000000001110011,	//ebreak
 };
 
-static int parse_args(int argc, char *argv[]) {
-	const struct option table[] = {
-		{"help"     , no_argument      , NULL, 'h'},
-		{0          , 0                , NULL,  0 },
-	};
-	int o;
-	while ( (o = getopt_long(argc, argv, "-h", table, NULL)) != -1) {
-		switch (o) {
-			case 1: img_file = optarg; return 0;
-			default:
-				printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
-                printf("\t-b,--batch              run with batch mode\n");
-                printf("\t-l,--log=FILE           output log to FILE\n");
-                printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
-                printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
-                printf("\t-f,--ftrace=FILE        parse the elf file\n");
-                printf("\n");
-                exit(0);
-		}
-	}
-	return 0;
-}
+// static int parse_args(int argc, char *argv[]) {
+// 	const struct option table[] = {
+// 		{"help"     , no_argument      , NULL, 'h'},
+// 		{0          , 0                , NULL,  0 },
+// 	};
+// 	int o;
+// 	while ( (o = getopt_long(argc, argv, "-h", table, NULL)) != -1) {
+// 		switch (o) {
+// 			case 1: img_file = optarg; return 0;
+// 			default:
+// 				printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+//                 printf("\t-b,--batch              run with batch mode\n");
+//                 printf("\t-l,--log=FILE           output log to FILE\n");
+//                 printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+//                 printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+//                 printf("\t-f,--ftrace=FILE        parse the elf file\n");
+//                 printf("\n");
+//                 exit(0);
+// 		}
+// 	}
+// 	return 0;
+// }
 
-static size_t load_img(uint32_t *memory){
-	printf("image file is %s\n", img_file);
-	if (img_file == NULL) {
-		printf("No image is given. Use the default build-in image.\n");
-    	return 4096; // built-in image size
-	}
+// static size_t load_img(uint32_t *memory){
+// 	printf("image file is %s\n", img_file);
+// 	if (img_file == NULL) {
+// 		printf("No image is given. Use the default build-in image.\n");
+//     	return 4096; // built-in image size
+// 	}
 
-	FILE *fp = fopen(img_file, "rb");
-	assert(fp);
+// 	FILE *fp = fopen(img_file, "rb");
+// 	assert(fp);
 
-	fseek(fp, 0, SEEK_END);
-	size_t size = ftell(fp);
+// 	fseek(fp, 0, SEEK_END);
+// 	size_t size = ftell(fp);
 
-	printf("The image is %s, size = %ld\n", img_file, size);
+// 	printf("The image is %s, size = %ld\n", img_file, size);
 
-	fseek(fp, 0 , SEEK_SET);
-	printf("Reading.....\n");
-	int ret = fread(guest_to_host(memory, 0x80000000), size, 1, fp);
-	printf("Read successfully!!\n");
-	assert(ret == 1);
+// 	fseek(fp, 0 , SEEK_SET);
+// 	printf("Reading.....\n");
+// 	int ret = fread(guest_to_host(memory, 0x80000000), size, 1, fp);
+// 	printf("Read successfully!!\n");
+// 	assert(ret == 1);
 
-	fclose(fp);
-	return size;
-}
+// 	fclose(fp);
+// 	return size;
+// }
 
 uint32_t *init_mem(size_t size) {
 	uint32_t* memory = (uint32_t*)malloc(size * sizeof(uint32_t));
