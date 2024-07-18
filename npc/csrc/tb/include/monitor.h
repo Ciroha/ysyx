5fc1cad80f8e30bbdcf5240d827a6390a900bf3a
tracer-ysyx <tracer@ysyx.org>
2024-07-18 18:20:07 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:20:07 up  7:41,  1 user,  load average: 0.27, 0.26, 0.25

--- npc/csrc/tb/include/monitor.h
+++ npc/csrc/tb/include/monitor.h
@@ -1,3 +1,6 @@
+#ifndef __MONITOR_H__
+#define __MONITOR_H__
+
 #include "stdint.h"
 #include "stdlib.h"
 #include "getopt.h"
@@ -16,3 +19,5 @@ uint32_t *init_mem(size_t size);
 
 
 uint32_t pmem_read(uint32_t *memory, uint32_t vaddr);
+
+#endif
--- npc/script/sim.mk
+++ npc/script/sim.mk
@@ -1,6 +1,7 @@
 #testbench
 CTB = $(shell find $(abspath $(NPC_HOME)/csrc/tb) -name "*.c" -or -name "*.cc" -or -name "*.cpp")
 VERILATOR_FLAGS += -CFLAGS "-I$(NPC_HOME)/csrc/tb/include"
+
 #sim
 sim: $(VSRCS) $(CTB)
 	$(call git_commit, "sim RTL") # DO NOT REMOVE THIS LINE!!!
@@ -9,6 +10,4 @@ sim: $(VSRCS) $(CTB)
 	$(VERILATOR) $(VERILATOR_FLAGS) \
 		--top-module $(TOPNAME) $^ \
 		--Mdir $(OBJ_DIR) --exe -o $(abspath $(BIN))
-
-run: sim	
-	$(BIN) $(IMG)
+# $(BIN) +trace
