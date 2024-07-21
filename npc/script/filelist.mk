fd4d99ed2887eaae2795debfde5f01ba822b8f05
tracer-ysyx <tracer@ysyx.org>
2024-07-21 17:45:31 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:45:31 up  8:09,  0 users,  load average: 0.34, 0.27, 0.21

--- npc/script/filelist.mk
+++ npc/script/filelist.mk
@@ -1,3 +1,4 @@
-CXXSRC = src/utils/disasm.cc
-CXXFLAGS += $(shell llvm-config --cxxflags) -fPIE
-LIBS += $(shell llvm-config --libs)
+# CXXSRC = src/utils/disasm.cc
+# CXXFLAGS += $(shell llvm-config --cxxflags) -fPIE
+# LIBS += $(shell llvm-config --libs)
+LDFLAGS += $(shell llvm-config --libs)
