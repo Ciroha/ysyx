7702a821245fcff2dec2225a75f9f1ac05f551e6
tracer-ysyx <tracer@ysyx.org>
2024-07-21 17:27:58 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:27:58 up  7:52,  0 users,  load average: 0.08, 0.13, 0.17

--- npc/script/sim.mk
+++ npc/script/sim.mk
@@ -3,7 +3,7 @@ CTB = $(shell find $(abspath $(NPC_HOME)/csrc/tb) -name "*.c" -or -name "*.cc" -
 VERILATOR_FLAGS += -CFLAGS "-I$(NPC_HOME)/csrc/tb/include"
 VERILATOR_FLAGS += -LDFLAGS "$(LDFLAGS) -lreadline"
 # LDFLAGS += $(shell llvm-config --ldflags)
-# LDFLAGS += $(shell llvm-config --libs)
+LDFLAGS += $(shell llvm-config --libs)
 LIBS += $(shell llvm-config --libs)
 
 #sim
