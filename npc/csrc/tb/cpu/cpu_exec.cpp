b45416d3421ea9025d5928bbe91e47a3158d94b6
tracer-ysyx <tracer@ysyx.org>
2024-07-22 12:41:54 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:41:54 up  1:51,  0 users,  load average: 0.42, 0.29, 0.27

--- npc/csrc/tb/cpu/cpu_exec.cpp
+++ npc/csrc/tb/cpu/cpu_exec.cpp
@@ -59,7 +59,7 @@ void reset(int n) {
 static void execute(uint32_t n) {
     for (; n > 0; n --) {
         single_cycle();
-        char buf[65536];
+        char buf[10];
         char *p = buf;
         p += sprintf(buf, "%s" "0x%08" "x" ": %08x ", "     ", cpu.pc, cpu.inst);
         int ilen = 4;
