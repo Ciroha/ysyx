3cf496cef11d493ecea159dc63e41b7907d9d72b
tracer-ysyx <tracer@ysyx.org>
2024-07-22 11:47:43 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:47:43 up 57 min,  0 users,  load average: 0.36, 0.33, 0.31

--- npc/csrc/tb/cpu/cpu_exec.cpp
+++ npc/csrc/tb/cpu/cpu_exec.cpp
@@ -32,10 +32,10 @@ static void execute(uint32_t n) {
         char *p = buf;
         p += snprintf(p, sizeof(buf), "0x%08" "x" ":", cpu.pc);
         int ilen = 4;
-        uint8_t *inst = (uint8_t *)&cpu.inst;
-        for (int i = ilen - 1; i >= 0; i--) {
-            p += snprintf(p, 4, "%02x", inst[i]);
-        }
+        // uint8_t *inst = (uint8_t *)&cpu.inst;
+        // for (int i = ilen - 1; i >= 0; i--) {
+        //     p += snprintf(p, 4, "%02x", inst[i]);
+        // }
         memset(p, ' ', 1);
         p += 1;
         disassemble(p, buf + sizeof(buf) - p, cpu.pc, (uint8_t *)&cpu.inst, 4);
