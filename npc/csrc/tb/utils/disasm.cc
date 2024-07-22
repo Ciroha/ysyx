ab56984c2c3bd631c69a7040f3de71016e83b8d4
tracer-ysyx <tracer@ysyx.org>
2024-07-22 13:16:31 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:16:31 up  2:26,  0 users,  load average: 0.54, 0.28, 0.28

--- npc/csrc/tb/utils/disasm.cc
+++ npc/csrc/tb/utils/disasm.cc
@@ -105,5 +105,6 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
   int skip = s.find_first_not_of('\t');
   const char *p = s.c_str() + skip;
   assert((int)s.length() - skip < size);
-  strcpy(str, p);
+  strncpy(str, p, size - 1);
+  str[size - 1] = '\0';
 }
