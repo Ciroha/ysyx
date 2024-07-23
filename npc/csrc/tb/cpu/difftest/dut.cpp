d66f37534e866956453d4bb5c6c234056238ac0d
tracer-ysyx <tracer@ysyx.org>
2024-07-23 20:44:49 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:49 up  9:47,  0 users,  load average: 0.51, 0.34, 0.24

--- npc/csrc/tb/cpu/difftest/dut.cpp
+++ npc/csrc/tb/cpu/difftest/dut.cpp
@@ -7,12 +7,6 @@
 
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 uint32_t gpr[32];
-const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
 
 void (*ref_difftest_memcpy)(uint32_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
--- npc/csrc/tb/include/cpu.h
+++ npc/csrc/tb/include/cpu.h
@@ -3,6 +3,13 @@
 #include "verilated_vcd_c.h"
 #include "Vysyx_23060332_top.h"
 
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
 extern Vysyx_23060332_top cpu;
 
 void single_cycle();
