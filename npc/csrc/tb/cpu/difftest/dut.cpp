b6a298dfdd1770e66fef0268ae6b17306fa22e08
tracer-ysyx <tracer@ysyx.org>
2024-07-23 21:12:04 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:04 up 10:14,  0 users,  load average: 0.50, 0.30, 0.27

--- npc/csrc/tb/cpu/difftest/dut.cpp
+++ npc/csrc/tb/cpu/difftest/dut.cpp
@@ -7,6 +7,12 @@
 
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 uint32_t gpr[32];
+const char *regs1[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
 
 void (*ref_difftest_memcpy)(uint32_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
@@ -57,6 +63,10 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
     }
     if(flag == false){
         printf("ref-pc=%x,dut-pc=%x\n",ref->pc, pc);
+        for(int i = 0; i < 32; i++){
+            printf("ref-reg-%3s     %d\n",regs1[i],ref->gpr[i]);
+        }
+        printf("NPC:\n");
         isa_reg_display();
     }
 }
