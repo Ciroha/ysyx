0c3e54554cccd99de4e2b94d85a8372bef31f0a3
tracer-ysyx <tracer@ysyx.org>
2024-07-23 20:46:29 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:46:29 up  9:48,  0 users,  load average: 0.21, 0.27, 0.22

--- npc/csrc/tb/include/cpu.h
+++ npc/csrc/tb/include/cpu.h
@@ -3,7 +3,7 @@
 #include "verilated_vcd_c.h"
 #include "Vysyx_23060332_top.h"
 
-const char *regs[] = {
+char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
