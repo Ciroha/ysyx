28da4676d543db5c6cfca3a33ca1ff190e96281e
tracer-ysyx <tracer@ysyx.org>
2024-07-24 21:23:13 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:13 up 10:22,  0 users,  load average: 0.49, 0.35, 0.35

--- npc/vsrc/ysyx_23060332_exu.v
+++ npc/vsrc/ysyx_23060332_exu.v
@@ -68,7 +68,7 @@ always @(*) begin
                     mem_wen = `WriteEnable;
                     mem_wdata = reg_rdata2_i;
                     mem_waddr = op1 + op2;
-                    mem_wmask = 8'b00000001;
+                    mem_wmask = 8'b00001001;
                 end
                 default: ;
             endcase
