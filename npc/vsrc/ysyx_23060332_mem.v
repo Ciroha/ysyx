9fa757fb81300b4ff93d7f8b0877b11da8ef100b
tracer-ysyx <tracer@ysyx.org>
2024-07-27 14:53:50 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:53:50 up  3:30,  0 users,  load average: 1.09, 0.47, 0.33

--- npc/vsrc/ysyx_23060332_mem.v
+++ npc/vsrc/ysyx_23060332_mem.v
@@ -20,23 +20,28 @@ import "DPI-C" function void pmem_write(input int waddr, input int wdata, input 
 // assign mem_rdata = rdata;
 wire valid;
 assign valid = (mem_raddr >= 32'h80000000) && (mem_raddr <= 32'h87ffffff);
-reg [31:0] temp1, temp2;
+// reg [31:0] temp1, temp2;
 
 always @(*) begin 
     mem_rdata = `ZeroWord;
-    temp1 = `ZeroWord;
-    temp2 = `ZeroWord;
+    // temp1 = `ZeroWord;
+    // temp2 = `ZeroWord;
+    // if (valid) begin
+    //     if (mem_ren) begin
+    //         temp1 = pmem_read(mem_raddr);
+    //         temp2 = pmem_read(mem_raddr+4);
+    //         case (mem_raddr[1:0])
+    //             2'b00: mem_rdata = temp1;
+    //             2'b01: mem_rdata = {{temp2[7:0]},{temp1[31:8]}};
+    //             2'b10: mem_rdata = {{temp2[15:0]},{temp1[31:16]}};
+    //             2'b11: mem_rdata = {{temp2[23:0]},{temp1[31:24]}};
+    //             default: ;
+    //         endcase
+    //     end
+    // end
     if (valid) begin
         if (mem_ren) begin
-            temp1 = pmem_read(mem_raddr);
-            temp2 = pmem_read(mem_raddr+4);
-            case (mem_raddr[1:0])
-                2'b00: mem_rdata = temp1;
-                2'b01: mem_rdata = {{temp2[7:0]},{temp1[31:8]}};
-                2'b10: mem_rdata = {{temp2[15:0]},{temp1[31:16]}};
-                2'b11: mem_rdata = {{temp2[23:0]},{temp1[31:24]}};
-                default: ;
-            endcase
+            mem_rdata = pmem_read(mem_raddr);
         end
     end
 end
