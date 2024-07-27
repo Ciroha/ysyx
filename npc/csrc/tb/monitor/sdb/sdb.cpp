9b117f74373c18a9873c9f513d6bc6078e475ba8
tracer-ysyx <tracer@ysyx.org>
2024-07-27 16:50:22 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:50:22 up  5:26,  0 users,  load average: 0.30, 0.28, 0.27

--- npc/csrc/tb/monitor/sdb/sdb.cpp
+++ npc/csrc/tb/monitor/sdb/sdb.cpp
@@ -120,6 +120,10 @@ static int cmd_help(char *args) {
 }
 
 void sdb_mainloop() {
+
+    cmd_c();
+    return;
+
     for (char *str; (str = rl_gets()) != NULL; ) {
         char *str_end = str + strlen(str);
 
