84693357733d796dbe8f7187928fde826d279e97
tracer-ysyx <tracer@ysyx.org>
2024-07-27 14:38:58 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:38:58 up  3:15,  0 users,  load average: 0.45, 0.33, 0.23

--- npc/vsrc/ysyx_23060332_mem.v
+++ npc/vsrc/ysyx_23060332_mem.v
@@ -20,7 +20,7 @@ import "DPI-C" function void pmem_write(input int waddr, input int wdata, input 
 // assign mem_rdata = rdata;
 wire valid;
 assign valid = (mem_raddr >= 32'h80000000) && (mem_raddr <= 32'h87ffffff);
-wire [31:0] temp1, temp2;
+reg [31:0] temp1, temp2;
 
 always @(*) begin 
     mem_rdata = `ZeroWord;
