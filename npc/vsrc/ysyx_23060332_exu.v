4bd4df8a80436fb5f7e6c287b6278b3173ac8e4f
tracer-ysyx <tracer@ysyx.org>
2024-07-27 16:10:07 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:10:07 up  4:46,  0 users,  load average: 0.52, 0.25, 0.21

--- npc/vsrc/ysyx_23060332_exu.v
+++ npc/vsrc/ysyx_23060332_exu.v
@@ -167,8 +167,8 @@ always @(*) begin
                     jump_addr = (op1 != op2) ? (op1_jump + op2_jump) : `ZeroWord;                    
                 end
                 `INST_BGE: begin
-                    jump_en = (op1 >= op2) ? `JumpEnable : `JumpDisable;
-                    jump_addr = (op1 >= op2) ? (op1_jump + op2_jump) : `ZeroWord;
+                    jump_en = ($signed(op1) >= $signed(op2)) ? `JumpEnable : `JumpDisable;
+                    jump_addr = ($signed(op1) >= $signed(op2)) ? (op1_jump + op2_jump) : `ZeroWord;
                 end
                 default: ;
             endcase
