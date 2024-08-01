8bfe31780b2d6ad4d0d29fc62e9d2fd382eabc61
ysyx_23060332-曹逸飞 <1255378802@qq.com>
2024-08-02 00:10:03 +0800

    完成时钟部分并成功跑分！

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,78 +7,155 @@
 
 int itoa(unsigned int n, char *buf)
 {
-  int i;
-  if (n < 10)
-  {
-    buf[0] = n + '0';
-    buf[1] = '\0';
-    return 0;
-  }
-  itoa(n/10, buf);
-  for (i = 0; buf[i] != '\0'; i++); //找到末尾的位置
-  buf[i] = (n % 10) + '0';
-  buf[i+1] = '\0';
-  return 0;
+	int i;
+	if (n < 10)
+	{
+		buf[0] = n + '0';
+		buf[1] = '\0';
+		return 0;
+	}
+	itoa(n/10, buf);
+	for (i = 0; buf[i] != '\0'; i++); //找到末尾的位置
+	buf[i] = (n % 10) + '0';
+	buf[i+1] = '\0';
+	return 0;
 }
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+	va_list ap;
+	int n;
+	char buf[65];
+	char format[65];	//后续可能需要调大
+	int cnt = 0;
+	int format_cnt = 0;
+	bool in_format = false;
+	char *s;
+
+	va_start(ap, fmt);
+	while(*fmt != '\0')
+	{
+		if (*fmt == '%' || in_format == true)
+		{
+			in_format = true;
+			if (*fmt == '%') {
+				memset(format, '\0', sizeof(format));
+				format_cnt = 0;
+				fmt++;
+			}
+			switch (*fmt)
+			{
+				case 'd':
+				{
+					n = va_arg(ap, int);
+					if (n < 0)
+					{
+						putch('-');
+						cnt++;
+						n = -n;
+					}
+					itoa(n, buf);
+					if (format != NULL && (strlen(buf) < format[1])) {	//TODO 需要后续修改
+						for (int k=0; k < (format[1]-'0')-strlen(buf); k++) {
+							putch(format[0]);
+						}
+						// putch(format[0]);
+						// putch(format[1]);
+					}
+					for (int i = 0; i < strlen(buf); i++)
+					{
+						putch(*(buf+i));
+					}
+					cnt += strlen(buf);
+					in_format = false;
+					break;
+				}
+				case 's':
+				{
+					s = va_arg(ap, char *);
+					for (int j = 0; j < strlen(s); j++)
+					{
+						putch(*(s+j));
+					}
+					cnt += strlen(s);
+					in_format = false;
+					break;
+				}
+				default: {
+					format[format_cnt] = *fmt;
+					// putch(format[0]);
+					// putch('\n');
+					// putch(format[1]);
+					// putch('\n');
+					format_cnt++;
+					break;
+				}
+			}
+		}
+		else
+		{
+			putch(*fmt);
+			cnt++;
+		}
+		fmt++;
+	}
+	va_end(ap);
+
+	return cnt;
+	// panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	// panic("Not implemented");
+	int n,j;
+	char buf[65];
+	char *s;
+
+	while(*fmt != '\0') {
+		if (*fmt == '%') {
+			fmt++;
+			switch (*fmt) {
+				case 'd': {
+					n = va_arg(ap, int);
+					if (n < 0)
+					{
+						*out = '-';
+						out++;
+						n = -n;
+					}
+					itoa(n, buf);
+					memcpy(out, buf, strlen(buf));
+					out += strlen(buf);
+					break;
+				}
+				case 's': {
+					s = va_arg(ap, char *);
+					memcpy(out, s, strlen(s));
+					out += strlen(s);
+					break;
+				}
+				default:
+				break;
+			}
+		}
+		else {
+			*out = *fmt;
+			out++;
+		}
+		fmt++;
+	}
+	*out = '\0';
+	// va_end(ap);
+	j = strlen(out);
+	return j;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  int n,j;
-  char buf[65];
-  char *s;
-
-  va_start(ap, fmt);
-  while(*fmt != '\0')
-  {
-    if (*fmt == '%')
-    {
-      fmt++;
-      switch (*fmt)
-      {
-        case 'd':
-        {
-          n = va_arg(ap, int);
-          if (n < 0)
-          {
-            *out = '-';
-            out++;
-            n = -n;
-          }
-          itoa(n, buf);
-          memcpy(out, buf, strlen(buf));
-          out += strlen(buf);
-          break;
-        }
-        case 's':
-        {
-          s = va_arg(ap, char *);
-          memcpy(out, s, strlen(s));
-          out += strlen(s);
-          break;
-        }
-        default:
-          break;
-      }
-    }
-    else
-    {
-      *out = *fmt;
-      out++;
-    }
-    fmt++;
-  }
-  *out = '\0';
-  va_end(ap);
-  j = strlen(out);
-  return j;
+	va_list ap;
+	va_start(ap, fmt);
+	vsprintf(out, fmt, ap);
+	va_end(ap);
+	int j = strlen(out);
+	return j;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
