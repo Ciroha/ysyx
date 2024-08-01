22e09d2ed080df349a2914136c1ddae395f391dc
tracer-ysyx <tracer@ysyx.org>
2024-08-01 21:34:16 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:34:16 up  8:32,  0 users,  load average: 1.03, 1.04, 0.85

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,14 +1,12 @@
 #include <am.h>
 #include <nemu.h>
 
-int cnt = 0;
-
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
+  uptime->us = 0;
   uptime->us += 100;
-  // cnt += 0;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
