564a8ab04e2dde4c3d2b36d66ff0021db9cefe93
tracer-ysyx <tracer@ysyx.org>
2024-08-01 10:59:35 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:59:35 up 14 min,  0 users,  load average: 0.50, 0.37, 0.26

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -22,7 +22,60 @@ int itoa(unsigned int n, char *buf)
 }
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  int n,j;
+  char buf[65];
+  char *buf_out = NULL;
+  char *s;
+
+  va_start(ap, fmt);
+  while(*fmt != '\0')
+  {
+    if (*fmt == '%')
+    {
+      fmt++;
+      switch (*fmt)
+      {
+        case 'd':
+        {
+          n = va_arg(ap, int);
+          if (n < 0)
+          {
+            *buf_out = '-';
+            buf_out++;
+            n = -n;
+          }
+          itoa(n, buf);
+          memcpy(buf_out, buf, strlen(buf));
+          buf_out += strlen(buf);
+          break;
+        }
+        case 's':
+        {
+          s = va_arg(ap, char *);
+          memcpy(buf_out, s, strlen(s));
+          buf_out += strlen(s);
+          break;
+        }
+        default:
+          break;
+      }
+    }
+    else
+    {
+      *buf_out = *fmt;
+      buf_out++;
+    }
+    fmt++;
+  }
+  *buf_out = '\0';
+  va_end(ap);
+  j = strlen(buf_out);
+  while (*buf_out != '\0') {
+    putch(*buf_out);
+    buf_out++;
+  }
+  return j;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
