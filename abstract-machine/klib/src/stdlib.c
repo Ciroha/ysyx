eb3116b06932dc67bea366038c07739dc6781fe5
tracer-ysyx <tracer@ysyx.org>
2024-08-02 18:16:32 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:16:32 up  6:07,  1 user,  load average: 0.55, 0.97, 1.10

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,8 +4,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
-// char *temp = NULL;
-char *hbrk = NULL;
+char *temp = NULL;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
@@ -39,15 +38,9 @@ void *malloc(size_t size) {
 //   panic("Not implemented");
 // #endif
 //   return NULL;
-  if(hbrk == NULL) hbrk = (void *)ROUNDUP(heap.start, 8);
-  size  = (size_t)ROUNDUP(size, 8);
-  char *old = hbrk;
-  hbrk += size;
-  assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
-  for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
-    *p = 0;
-  }
-  return old;
+  if (temp == NULL) temp = heap.start;
+  memset(temp, '\0', size);
+  return temp;
 }
 
 void free(void *ptr) {
