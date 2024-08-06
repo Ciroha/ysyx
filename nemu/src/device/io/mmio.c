3e4ac64f53510c2f5d324ff7c5dc2febbe5b7ecc
tracer-ysyx <tracer@ysyx.org>
2024-08-06 22:27:10 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:27:10 up  5:29,  1 user,  load average: 0.39, 0.50, 0.38

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -55,11 +55,11 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 
 /* bus interface */
 word_t mmio_read(paddr_t addr, int len) {
-  Log("%s read from addr: %x", maps->name, addr);
+  Log("[%s] read from addr: %x", maps->name, addr);
   return map_read(addr, len, fetch_mmio_map(addr));
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
-  Log("%s write to addr: %x", maps->name, addr);
+  Log("[%s] write to addr: %x", maps->name, addr);
   map_write(addr, len, data, fetch_mmio_map(addr));
 }
