ef2fb905e2f3540bbe04bb89d81a4a4cbd858f95
tracer-ysyx <tracer@ysyx.org>
2024-08-11 18:12:22 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:12:22 up 50 min,  1 user,  load average: 0.34, 0.35, 0.39

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,10 +4,11 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
-  // if (kbd->keydown == true)
-  //   kbd->keycode = kbd->keycode | KEYDOWN_MASK;
-  // else
-  //   kbd->keycode = AM_KEY_NONE;
+  kbd->keycode = inl(KBD_ADDR);
+  if (kbd->keycode == AM_KEY_NONE || kbd->keycode & KEYDOWN_MASK) {
+    kbd->keydown = false;
+  } else {
+    kbd->keydown = true;
+    kbd->keycode = kbd->keycode & ~KEYDOWN_MASK;
+  }
 }
