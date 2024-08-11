dd2a7a04a07aabead7afbff6e8a1eedf8a054c35
tracer-ysyx <tracer@ysyx.org>
2024-08-11 18:33:33 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:33:33 up  1:11,  1 user,  load average: 0.18, 0.17, 0.21

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,10 +5,10 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keycode = inl(KBD_ADDR);
-  if(kbd->keycode == AM_KEY_NONE || (kbd->keycode >> 15) == 0)
-    kbd->keydown = 0;
-  else{
-    kbd->keydown = 1;
+  if (kbd->keycode == AM_KEY_NONE || kbd->keycode & KEYDOWN_MASK) {
+    kbd->keydown = false;
+  } else {
+    kbd->keydown = true;
     kbd->keycode = kbd->keycode & ~KEYDOWN_MASK;
-    }
+  }
 }
