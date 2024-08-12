bf78f009a6b25d72bee8d4e31f8b85d7c1192210
tracer-ysyx <tracer@ysyx.org>
2024-08-12 21:58:37 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:58:37 up  1:41,  1 user,  load average: 0.35, 0.59, 0.52

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -29,16 +29,16 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->w;
-  uint32_t *pixels = ctl->pixels;
-  uint32_t *fb = (uint32_t *)(uintptr_t)(FB_ADDR + (ctl->y*WIDTH + ctl->x) * sizeof(uint32_t));
-  for (int i = y; i < y + h; i++) {
-    for (int j = x; j < x + w; j++) {
-      fb[screen_w*i + j] = pixels[w * (i-y) + (j-x)];
-    }
-  }
+  int i,j;
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+  }
+  uint32_t *p = (uint32_t *)ctl->pixels; 
+  uint32_t *fb = (uint32_t *)(uintptr_t)(FB_ADDR + (ctl->y*WIDTH + ctl->x) * sizeof(uint32_t));
+  for(i = 0;i < ctl->h;i++){
+    for(j = 0;j < ctl->w;j++)
+      fb[j] = *p++;
+    fb += WIDTH;
   }
 }
 
