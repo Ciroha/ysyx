ead7273317d46b110bac60a8abee8c628ef1f931
tracer-ysyx <tracer@ysyx.org>
2024-08-15 23:28:53 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:28:53 up  3:39,  0 users,  load average: 0.27, 0.29, 0.27

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -68,6 +68,6 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     pmem_write(addr, len, data); 
     return; 
   }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); Log("paddr write"); return);
+  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
