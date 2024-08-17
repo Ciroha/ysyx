4ba226420fffa362bff3b93e205628b997b396f3
tracer-ysyx <tracer@ysyx.org>
2024-08-17 11:02:20 +0800

    > compile NEMU ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:02:20 up  1:31,  1 user,  load average: 0.54, 0.57, 0.46

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -25,15 +25,15 @@ void init_sdb();
 void init_disasm(const char *triple);
 void init_ftrace(char *elf_file);
 
-// static void welcome() {
-//   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
-//   IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
-//         "to record the trace. This may lead to a large log file. "
-//         "If it is not necessary, you can disable it in menuconfig"));
-//   Log("Build time: %s, %s", __TIME__, __DATE__);
-//   printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
-//   printf("For help, type \"help\"\n");
-// }
+static void welcome() {
+  Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
+  IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
+        "to record the trace. This may lead to a large log file. "
+        "If it is not necessary, you can disable it in menuconfig"));
+  Log("Build time: %s, %s", __TIME__, __DATE__);
+  printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
+  printf("For help, type \"help\"\n");
+}
 
 #ifndef CONFIG_TARGET_AM
 #include <getopt.h>
@@ -146,7 +146,7 @@ void init_monitor(int argc, char *argv[]) {
 #endif
 
   /* Display welcome message. */
-  // welcome();
+  welcome();
 }
 #else // CONFIG_TARGET_AM
 static long load_img() {
