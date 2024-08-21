332a7fba21668b0d37c414a3a1f28491087c3a40
tracer-ysyx <tracer@ysyx.org>
2024-08-21 22:56:08 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:56:08 up  1:44,  0 users,  load average: 0.52, 0.27, 0.20

--- npc/csrc/tb/utils/timer.cpp
+++ npc/csrc/tb/utils/timer.cpp
@@ -21,7 +21,7 @@ static uint64_t boot_time = 0;
 static uint64_t get_time_internal() {
   struct timeval now;
   gettimeofday(&now, NULL);
-  uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
+  uint64_t us = now.tv_sec * 3000000 + now.tv_usec;
   return us;
 }
 
