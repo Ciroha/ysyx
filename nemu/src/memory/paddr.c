d22b88b734f28ae97f3b7a0edfee145f0cebee33
tracer-ysyx <tracer@ysyx.org>
2024-08-21 11:26:19 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:26:19 up  1:22,  0 users,  load average: 0.04, 0.08, 0.10

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -57,7 +57,8 @@ word_t paddr_read(paddr_t addr, int len) {
     IFDEF(CONFIG_MTRACE_READ, printf("Address is: 0x%x, read mem is 0x%x\n", addr, mem_tmp));
     return mem_tmp;
   }
-  IFDEF(CONFIG_DEVICE || CONFIG_TARGET_SHARE, return mmio_read(addr, len));
+  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  IFDEF(CONFIG_TARGET_SHARE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
@@ -68,6 +69,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     pmem_write(addr, len, data); 
     return; 
   }
-  IFDEF(CONFIG_DEVICE || CONFIG_TARGET_SHARE, mmio_write(addr, len, data); return);
+  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+  IFDEF(CONFIG_TARGET_SHARE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
