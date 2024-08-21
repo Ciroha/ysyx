c47bd71fc2af1e5c3ff8d0de5272824508bc5115
tracer-ysyx <tracer@ysyx.org>
2024-08-21 16:50:21 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:50:21 up  6:46,  0 users,  load average: 0.52, 0.33, 0.31

--- npc/csrc/tb/memory/memory.cpp
+++ npc/csrc/tb/memory/memory.cpp
@@ -28,8 +28,8 @@ void init_mem(size_t size) {
 extern "C" int pmem_read(int raddr){
 	time_t current_time;
 	time(&current_time);
-	if (raddr == 0xa0000048) {return (current_time&&0xffffffff);}
-	if (raddr == 0xa000004c) {return (current_time>>32);}
+	if (raddr == 0xa0000048) {Log("Address is: %#010x", raddr);return (current_time&&0xffffffff);}
+	if (raddr == 0xa000004c) {Log("Address is: %#010x", raddr);return (current_time>>32);}
 	// Log("Address is: %#010x", raddr);
 	int temp = raddr & ~0x3u;
 	int ret = host_read(guest_to_host(temp), 4);
