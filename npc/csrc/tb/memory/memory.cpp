8c14e1bb699fede4799bb029976ce243beb78b76
tracer-ysyx <tracer@ysyx.org>
2024-08-21 10:33:24 +0800

    > sim RTL ysyx_23060332 曹逸飞 Linux ciroha 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:33:24 up 30 min,  0 users,  load average: 0.41, 0.28, 0.19

--- npc/csrc/tb/memory/memory.cpp
+++ npc/csrc/tb/memory/memory.cpp
@@ -25,10 +25,12 @@ void init_mem(size_t size) {
 }
 
 extern "C" int pmem_read(int raddr){
+	// if (raddr == ) {}
 	int temp = raddr & ~0x3u;
-	// Log("Address is: %#010x", temp);
 	int ret = host_read(guest_to_host(temp), 4);
-	// Log("Address is: %#010x, read mem is %#010x", temp, ret);
+#ifdef CONFIG_MTRACE_READ
+	Log("Address is: %#010x, read mem is %#010x", temp, ret);
+#endif
 	return ret;
 }
 
@@ -36,6 +38,7 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask) {
   	// 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
   	// `wmask`中每比特表示`wdata`中1个字节的掩码,
   	// 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
+	if (waddr == 0xa00003f8) {putchar(wdata);}
 	int temp = waddr & ~0x3u;
 #ifdef CONFIG_MTRACE_WRITE
 	Log("Address is: %#010x, write mem is %#010x, mask is %d", temp, wdata, wmask);
